fb9d78c23e0645d4d0f28686afeae2b5f6f12791
Alex <movilra@gmail.com>
2018-04-19 19:51:53 -0500

    Mi primer trabajo

---
+++ index.js
@@ -0,0 +1,18 @@
+var hola="hola"
+function decirHola(){
+    console.log(hola)
+}
+
+/*git commit
+
+git push
+
+leave building
+
+-----
+git init 
+git add.
+git status
+git pushgit add.
+git commit -m "comentarios"
+git push */
